c20282e0f92b939fc37e4f3ec11970454924be68
Kyrian Obikwelu <koshnawaza@gmail.com>
2026-05-22 00:20:28 +0100

    fix: use the static ffi build for windows instead

--- build/win.sh
+++ build/win.sh
@@ -24,7 +24,7 @@ case ${PLATFORM} in
     ;;
 esac
 
-FILENAME="vips-dev-${ARCH}-web-${VERSION_VIPS}-static.zip"
+FILENAME="vips-dev-${ARCH}-web-${VERSION_VIPS}-static-ffi.zip"
 URL="https://github.com/libvips/build-win64-mxe/releases/download/v${VERSION_VIPS}/${FILENAME}"
 echo "Downloading $URL"
 $CURL -O $URL
